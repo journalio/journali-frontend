f0b52ab33797a00644a1387b50db2240d3d7c24c
Wesley Klop <wesley19097@gmail.com>
2020-04-07 16:10:51 +0200

    Add application title to the index page

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-console.log('test')
+import Vue from 'vue'
+import AppTitle from './AppTitle'
+
+import './style.css'
+
+new Vue({
+    el: '#app',
+    render: (h) => h(AppTitle),
+})
+
+// EErst even lokaal testen!!
