91c71039062026655314d86e5db71d8e455dae56
Wesley Klop <wesley19097@gmail.com>
2020-04-14 11:55:33 +0200

    Calm down

--- app/JournaliApp.vue
+++ app/JournaliApp.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container mx-auto pt-4">
         <h1 class="font-bold text-3xl text-yellow-500 py-2">
-            Journali is geweldig!!!
+            Journali is geweldig!
         </h1>
 
         <router-link to="/" class="text-yellow-500">
