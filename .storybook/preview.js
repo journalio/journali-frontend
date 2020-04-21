0997e4192e5bcf4d12dc477d0218bacece39cd7d
Wesley Klop <wesley19097@gmail.com>
2020-04-21 13:13:32 +0200

    Add padding decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,8 @@
+import { addDecorator } from '@storybook/vue'
 import '../app/style.css'
+
+const PaddingDecorator = () => ({
+    template: `<div class="p-4"><story /></div>`,
+})
+
+addDecorator(PaddingDecorator)
--- app/components/TestButton.vue
+++ app/components/TestButton.vue
@@ -1,14 +1,14 @@
-<template>
-    <button
-        @click="$emit('click', $event)"
-        class="rounded bg-blue-600 hover:bg-blue-800 py-1 px-4 text-white"
-    >
-        <slot></slot>
-    </button>
-</template>
-
 <script>
 export default {
     name: 'TestButton',
 }
 </script>
+
+<template>
+    <button
+        class="rounded bg-blue-600 hover:bg-blue-800 py-1 px-4 text-white"
+        @click="$emit('click', $event)"
+    >
+        <slot></slot>
+    </button>
+</template>
