df422a30a1bb6fb32e303c2588bed2e4928f6970
Wesley Klop <wesley19097@gmail.com>
2020-04-22 22:51:02 +0200

    Add tailwind and proxy to backend

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import './assets/tailwind.css'
 import router from './router'
 import store from './store'
-
-Vue.config.productionTip = false
 
 new Vue({
     router,
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     lintOnSave: process.env.NODE_ENV !== 'production',
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        proxy: {
+            '^/api': {
+                target: 'http://api:8000',
+            },
+        },
+    },
 }
