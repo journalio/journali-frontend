5a7018e354a26e03ca011b734783d90d1ddeaa69
unknown <rdenblaauwen@gmail.com>
2020-05-12 15:43:44 +0200

    basic login function should be working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" class="mx-auto" src="../assets/logo.png" />
+        <div>
+            <form class="flex flex-col items-center" @submit.prevent="login">
+                <label for="username">Username</label>
+                <input
+                    v-model="username"
+                    type="test"
+                    name="username"
+                    class="text-black bg-gray-200 px-2 py-1 border-2 rounded"
+                />
+
+                <label for="password">Password</label>
+                <input
+                    v-model="password"
+                    type="password"
+                    name="password"
+                    class="text-black bg-gray-200 px-2 py-1 border-2 rounded"
+                />
+
+                <input
+                    class="my-4 rounded px-4 py-1 bg-white text-gray-800 border-gray-800 border font-bold uppercase w-24"
+                    type="submit"
+                    value="submit"
+                />
+            </form>
+        </div>
+
         <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
 </template>
@@ -13,6 +38,33 @@ export default {
     name: 'Home',
     components: {
         HelloWorld,
+    },
+    data() {
+        return {
+            username: '',
+            password: '',
+        }
+    },
+    methods: {
+        async login() {
+            // const data = {
+            //     username: this.username,
+            //     password: this.password,
+            // }
+            // const response = await fetch(`/api/login/`, { body: data })
+            // const responseObject = response.json()
+
+            // testing purposes, pls remove
+            const responseObject = {
+                token: this.password,
+            }
+
+            if (responseObject.token) {
+                const storage = window.localStorage
+                storage.setItem('journali-token', responseObject.token)
+                this.$router.push('hello-world')
+            }
+        },
     },
 }
 </script>
