b12e5e85e5a1298deb319d08568316f3f14b2df7
Wesley Klop <wesley19097@gmail.com>
2020-05-16 16:40:00 +0200

    Forgot to remove global httpclient

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import './assets/tailwind.css'
-import AbstractHttpClient from './lib/http/AbstractHttpClient'
 import router from './router'
 import store from './store'
 
 console.log(
     `Running version "${process.env.VUE_APP_VERSION}" in "${process.env.NODE_ENV}"`,
 )
-
-// add library to Vue globally
-Vue.prototype.$http = new AbstractHttpClient()
 
 new Vue({
     router,
--- src/shims.d.ts
+++
@@ -1,8 +0,0 @@
-// may need some polishing: https://vuejs.org/v2/guide/typescript.html
-import AbstractHttpClient from '@/lib/http/AbstractHttpClient'
-
-declare module 'vue/types/vue' {
-    interface Vue {
-        $http: AbstractHttpClient
-    }
-}
