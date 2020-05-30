6003f221e1919febbf70fb1bf6788d806f977074
unknown <rdenblaauwen@gmail.com>
2020-05-30 16:04:34 +0200

    filtering pages now works, needs a few design changes though

--- src/components/JournaliSidebar.vue
+++ src/components/JournaliSidebar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="w-auto bg-white shadow py-8 z-10">
-        <div class="text-sm mb-1 text-gray-700 px-4">Pages</div>
+        <page-searcher class="px-4" @resultsFound="foundPages = $event" />
+        <div class="text-sm my-2 text-gray-700 px-4">Pages</div>
         <pages-list :pages="pages" @delete="deletePage($event)"></pages-list>
         <page-adder class="px-4 mt-4" @add:page="openPage($event)" />
     </div>
@@ -9,16 +10,21 @@
 <script lang="ts">
 import PageAdder from '@/components/PageAdder.vue'
 import PagesList from '@/components/PagesList.vue'
+import PageSearcher from '@/components/PageSearcher.vue'
 import { Page } from '@/models'
 import { Uuid } from '@/models/types'
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
-    components: { PagesList, PageAdder },
+    components: { PagesList, PageAdder, PageSearcher },
 })
 export default class JournaliSidebar extends Vue {
+    foundPages = []
+
     get pages() {
-        return this.$store.state.pages
+        return this.foundPages.length > 0
+            ? this.foundPages
+            : this.$store.state.pages
     }
 
     protected mounted() {
