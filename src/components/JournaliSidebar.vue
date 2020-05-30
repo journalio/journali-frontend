6fb0635a1db88d65b51fc83ec6ab3c6dd971cc5e
unknown <rdenblaauwen@gmail.com>
2020-05-30 16:16:32 +0200

    moved retrieving pages to journaliSidebar, PageSearcher -> PageFilter

--- src/components/JournaliSidebar.vue
+++ src/components/JournaliSidebar.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="w-auto bg-white shadow py-8 z-10">
-        <page-searcher class="px-4" @resultsFound="foundPages = $event" />
+        <page-filter
+            :pages="pages"
+            class="px-4"
+            @resultsFound="foundPages = $event"
+        />
         <div class="text-sm my-2 text-gray-700 px-4">Pages</div>
-        <pages-list :pages="pages" @delete="deletePage($event)"></pages-list>
+        <pages-list
+            :pages="pagesToDisplay"
+            @delete="deletePage($event)"
+        ></pages-list>
         <page-adder class="px-4 mt-4" @add:page="openPage($event)" />
     </div>
 </template>
@@ -10,21 +17,23 @@
 <script lang="ts">
 import PageAdder from '@/components/PageAdder.vue'
 import PagesList from '@/components/PagesList.vue'
-import PageSearcher from '@/components/PageSearcher.vue'
+import PageFilter from '@/components/PageFilter.vue'
 import { Page } from '@/models'
 import { Uuid } from '@/models/types'
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
-    components: { PagesList, PageAdder, PageSearcher },
+    components: { PagesList, PageAdder, PageFilter },
 })
 export default class JournaliSidebar extends Vue {
     foundPages = []
 
     get pages() {
-        return this.foundPages.length > 0
-            ? this.foundPages
-            : this.$store.state.pages
+        return this.$store.state.pages
+    }
+
+    get pagesToDisplay() {
+        return this.foundPages.length > 0 ? this.foundPages : this.pages
     }
 
     protected mounted() {
