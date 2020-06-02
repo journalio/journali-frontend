11231bf9de50f421e23b912562fb51490c7de7b8
unknown <rdenblaauwen@gmail.com>
2020-06-02 11:38:18 +0200

    Simplified filtering a lot

--- src/components/JournaliSidebar.vue
+++ src/components/JournaliSidebar.vue
@@ -1,40 +1,42 @@
 <template>
     <div class="w-auto bg-white shadow py-8 z-10">
-        <page-filter
-            :pages="pages"
+        <text-input
+            v-model="searchWord"
+            autocomplete="off"
             class="px-4"
-            @resultsFound="foundPages = $event"
+            label="Search"
+            placeholder="Key words"
+            type="search"
         />
         <div class="text-sm my-2 text-gray-700 px-4">Pages</div>
-        <pages-list
-            :pages="pagesToDisplay"
-            @delete="deletePage($event)"
-        ></pages-list>
+        <pages-list :pages="pages" @delete="deletePage($event)"></pages-list>
         <page-adder class="px-4 mt-4" @add:page="openPage($event)" />
     </div>
 </template>
 
 <script lang="ts">
+import TextInput from '@/components/TextInput.vue'
 import PageAdder from '@/components/PageAdder.vue'
 import PagesList from '@/components/PagesList.vue'
-import PageFilter from '@/components/PageFilter.vue'
 import { Page } from '@/models'
 import { Uuid } from '@/models/types'
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
-    components: { PagesList, PageAdder, PageFilter },
+    components: { PagesList, PageAdder, TextInput },
 })
 export default class JournaliSidebar extends Vue {
-    foundPages = []
+    searchWord = ''
 
     get pages() {
-        return this.$store.state.pages
+        return this.$store.state.pages.filter((page: Page) =>
+            page.title.toLowerCase().includes(this.searchWord.toLowerCase()),
+        )
     }
 
-    get pagesToDisplay() {
-        return this.foundPages.length > 0 ? this.foundPages : this.pages
-    }
+    // get pagesToDisplay() {
+    //     return this.foundPages.length > 0 ? this.foundPages : this.pages
+    // }
 
     protected mounted() {
         this.$store.dispatch('loadPages')
